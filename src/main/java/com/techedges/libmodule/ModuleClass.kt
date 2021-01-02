72182d32d70cd20a0a5c193c6c77a27ca9df6958
Maulana Kahfi <naufalazzaahid@gmail.com>
2021-01-02 16:44:45 +0700

    try changing from sub module

--- src/main/java/com/techedges/libmodule/ModuleClass.kt
+++ src/main/java/com/techedges/libmodule/ModuleClass.kt
@@ -5,4 +5,4 @@ package com.techedges.libmodule
  * Created by Muhammad Naufal Azzaahid on 28/12/2020.
  */
 
-data class ModuleClass(val nameSample: String = "defaul")
+data class ModuleClass(val nameSample: String = "default")
